0c83995af1ba9ed7c93bc08ba671dd92cd1f444a
Ali Sheikh <ali.sheikh@46labs.com>
2021-08-18 18:35:23 +0500

    Signed-off-by: Ali Sheikh <ali.sheikh@46labs.com>

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module BrandApp
       end
     end
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
